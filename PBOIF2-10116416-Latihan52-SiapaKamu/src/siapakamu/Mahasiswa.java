7fe63c2b6bb7fbdef4b1c7f9dd983c3db8d39d87
baharriyan <73172116+baharriyan@users.noreply.github.com>
2020-11-22 19:10:58 +0700

    Add files via upload

---
+++ PBOIF2-10116416-Latihan52-SiapaKamu/src/siapakamu/Mahasiswa.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package siapakamu;
+
+/**
+ *Nama  : Riyan Bahar
+ *Kelas : PBO-02
+ *NIM   : 10116416
+ */
+class Mahasiswa extends Manusia {
+
+private String nim;
+    private String kelas;
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getKelas() {
+        return kelas;
+    }
+
+    public void setKelas(String kelas) {
+        this.kelas = kelas;
+    }
+
+    public void kelasApa() {
+        System.out.println("Saya " + nama + " umur " + umur
+                + " tahun sedang belajar di kelas " + kelas);
+    }
+
+    public void siapaKamu() {
+        System.out.println("Saya Mahasiswa");
+    }
+    
+}
